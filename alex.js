8639701a25f29a06f361953c65c88c2f4096d9fb
Alex-724 <alex000824@gmail.com>
2022-01-29 21:29:15 +0300

    Hello

--- alex.js
+++ alex.js
@@ -3,21 +3,24 @@ const client = new Discord.Client();
 
 async function login(token){
   client.login(token);
+  console.log(`login as ${client.user.tag}`)
 }
 
 async function joinVoice(id){
   client.on("READY", async ready => {
     let channel = client.channels.cache.get(id)
-    if (!channel) throw new Error("You need to type voice channel id");
-    channel.login;
+    if (!channel) return console.error("You need to type voice channel id");
+    channel.join();
+    console.log(`join ${channel.name}`)
   })
 }
 
 async function setStatus(statu){
   client.on("READY", async ready => {
-    if (!statu) throw new Error("You need to type status between `online`,`idle`,`dnd`")
-    if (statu !== "online" || statu !== "ide" || statu !== "dnd") throw new Error("You need to type status between `online`,`idle`,`dnd`")
-    client.user.setPresence({status: statu[1]});
+    if (!statu) return console.error("You need to type status between `online`,`idle`,`dnd`")
+    if (statu !== "online" || statu !== "ide" || statu !== "dnd") return console.error("You need to type status between `online`,`idle`,`dnd`")
+    client.user.setPresence({status: statu[1]})
+    console.log(`status is ${statu}`)
   })
 }
 
