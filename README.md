84076c7fe6679b11ef5e05434e9ba04efe0bf7bd
Alex-724 <alex000824@gmail.com>
2022-02-01 01:04:36 +0300

    aadd selfmute

--- README.md
+++ README.md
@@ -12,15 +12,19 @@ const Alex = require('discord.js-selfbot-alex')
 ```js
 Alex.login(token)
 ```
+
 ## Fast Demo
 ```js
 const Alex = require('discord.js-selfbot-alex');
 Alex.start({
     token: "false", // You must type account token here
-    status: "idle", // You need to type account statues here if you don't need type "false"
-    channel_id: "12345678" // You need to type voice channel id here if you don't need type "false"
-    mute: "true" // you can add self mute if you don't need type "false"
-    def: "true" // you can add self defend if you don't need type "false"
+    status: "idle", // You need to type account statues here if you don't need it type "false"
+    channel_id: "12345678", // You need to type voice channel id here if you don't need it type "false"
+    mute: "true", // you can add self mute if you don't need it type "false"
+    def: "true", // you can add self defend if you don't need it type "false"
+    akf: "false", // this is akf message for selfbot
+    akf_dm_message: "hello i don't have a time", //send akf message in dm if you don't need it type "false"
+    akf_guild_message: "hello i don't have a time" //send akf message in guild if you don't need it type "false"
 })
 ```
 
--- alex.js
+++ alex.js
@@ -1,7 +1,9 @@
 const Discord = require("discord.js-selfbot")
-const client = new Discord.Client();
-
+const main_client = new Discord.Client();
+const newLine = `
+`;
 async function login(token) {
+  const client = new Discord.Client();
   client.login(token);
   client.on("ready", async () => {
     console.log(`login as ${client.user.tag}`)
@@ -27,17 +29,22 @@ async function setStatus(statu) {
 }
 
 async function start(data) {
+  const console = "";
+
   const TOKEN = data.token,
     STATUS = data.status,
     CHANNEL_ID = data.channel_id,
     MUTE = data.mute,
-    DEF = data.def;
+    DEF = data.def,
+    AKF = data.akf,
+    AKF_DM_MESSAGE = data.akf_dm_message,
+    AKF_GUILD_MESSAGE = data.akf_guild_message;
   ////////////////////////////////////////////////////////////////////////////////
   const alex = new Discord.Client();
   if (!TOKEN) return console.error("You need to type token")
   alex.login(TOKEN);
   alex.on("ready", async () => {
-    console.log(`login as ${alex.user.tag}`)
+    console = log(console,`login as ${alex.user.tag}`,"log")
   })
   ////////////////////////////////////////////////////////////////////////////////
   alex.on("ready", async () => {
@@ -45,7 +52,7 @@ async function start(data) {
     if (!STATUS) return console.error("You need to type status between `online`,`idle`,`dnd`")
     if (STATUS !== "online" && STATUS !== "idle" && STATUS !== "dnd") return console.error("You need to type status between `online`,`idle`,`dnd`")
     alex.user.setPresence({ status: STATUS })
-    console.log(`status is ${STATUS}`)
+    console = log(console,`status is ${STATUS}`,"log")
   })
   ////////////////////////////////////////////////////////////////////////////////
   alex.on("ready", async () => {
@@ -54,26 +61,61 @@ async function start(data) {
     let channel = alex.channels.cache.get(CHANNEL_ID)
     if (!channel) return console.error("You need to type voice channel id");
     channel.join().then(connection => {
+      console = log(console,`join ${channel.name}`,"log")
       ////////////////////
       if (DEF === "true") {
+        console = log(console,`account is defend`,"log")
         connection.voice.setSelfDeaf(true);
       } else if (DEF === "false") {
+        console = log(console,`account is undefend`,"error")
         connection.voice.setSelfDeaf(false);
       }
       ////////////////////
       if (MUTE === "true") {
+        console = log(console,`account is mute`,"log")
         connection.voice.setSelfMute(true);
       } else if (MUTE === "false") {
+        console = log(console,`account is unmute`,"error")
         connection.voice.setSelfMute(false);
       }
-      console.log(`join ${channel.name}`);
     })
       .catch(e => {
         console.error(e);
       });
   })
+  ////////////////////////////////////////////////////////////////////////////////
+  alex.on("message", async message => {
+    if (!AKF || AKF !== "true") return;
+    if (message.author.bot) return;
+    if (!message.guild) {
+      if (!AKF_DM_MESSAGE || AKF_DM_MESSAGE !== "true") return;
+      message.reply(`AKF: ${AKF_DM_MESSAGE}`).catch(error => {})
+    } else if (message.guild) {
+      if (!AKF_GUILD_MESSAGE || AKF_GUILD_MESSAGE !== "true") return;
+      message.reply(`AKF: ${AKF_GUILD_MESSAGE}`).catch(error => {})
+    } else {
+      return console.error("something went wrong in my code");
+    }
+  })
+  alex.on("ready", async () => {
+    if (!AKF || AKF !== "true") return;
+    if (AKF_DM_MESSAGE || AKF_DM_MESSAGE === "true") {
+      console = log(console,`You have enabled akf dm message and the message is ${AKF_DM_MESSAGE}`,"log")
+    } else if (AKF_GUILD_MESSAGE || AKF_GUILD_MESSAGE === "true") {
+      console = log(console,`You have enabled akf guild message and the message is ${AKF_GUILD_MESSAGE}`,"log")
+    };
+  })
+  console.log(console)
 }
 
+async function log(oldmsg, addmsg, cc) {
+  if (cc === "log") {
+    oldmsg = oldmsg + "🟢" + addmsg + newLine;
+  } else if (cc === "error") {
+    oldmsg = oldmsg + "⛔" + addmsg;
+  }
+  return oldmsg
+}
 module.exports.start = start
 module.exports.login = login
 module.exports.setStatus = setStatus
