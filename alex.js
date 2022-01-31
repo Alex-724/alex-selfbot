73b4aaf4bc6b59579186d077cd45b3aa4aa8621b
Alex-724 <alex000824@gmail.com>
2022-02-01 02:20:31 +0300

    67y5

--- alex.js
+++ alex.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js-selfbot")
 const main_client = new Discord.Client();
-const newLine = `
-`;
+const newLine = "\n";
 async function login(token) {
   const client = new Discord.Client();
   client.login(token);
@@ -29,8 +28,7 @@ async function setStatus(statu) {
 }
 
 async function start(data) {
-  const console = "";
-
+  let msssg = "";
   const TOKEN = data.token,
     STATUS = data.status,
     CHANNEL_ID = data.channel_id,
@@ -44,78 +42,84 @@ async function start(data) {
   if (!TOKEN) return console.error("You need to type token")
   alex.login(TOKEN);
   alex.on("ready", async () => {
-    console = log(console,`login as ${alex.user.tag}`,"log")
-  })
+    msssg += `[🟢] login as ${alex.user.tag}` + newLine;
+  });
   ////////////////////////////////////////////////////////////////////////////////
   alex.on("ready", async () => {
     if (STATUS === "false") return;
-    if (!STATUS) return console.error("You need to type status between `online`,`idle`,`dnd`")
-    if (STATUS !== "online" && STATUS !== "idle" && STATUS !== "dnd") return console.error("You need to type status between `online`,`idle`,`dnd`")
-    alex.user.setPresence({ status: STATUS })
-    console = log(console,`status is ${STATUS}`,"log")
-  })
+    if (!STATUS)
+      return console.error(
+        "You need to type status between `online`,`idle`,`dnd`"
+      );
+    if (STATUS !== "online" && STATUS !== "idle" && STATUS !== "dnd")
+      return console.error(
+        "You need to type status between `online`,`idle`,`dnd`"
+      );
+    alex.user.setPresence({ status: STATUS });
+    msssg += `[🟢] status is ${STATUS}` + newLine;
+  });
   ////////////////////////////////////////////////////////////////////////////////
   alex.on("ready", async () => {
     if (CHANNEL_ID === "false") return;
-    if (!CHANNEL_ID) return console.error("You need to type voice channel id")
-    let channel = alex.channels.cache.get(CHANNEL_ID)
+    if (!CHANNEL_ID) return console.error("You need to type voice channel id");
+    let channel = alex.channels.cache.get(CHANNEL_ID);
     if (!channel) return console.error("You need to type voice channel id");
-    channel.join().then(connection => {
-      console = log(console,`join ${channel.name}`,"log")
-      ////////////////////
-      if (DEF === "true") {
-        console = log(console,`account is defend`,"log")
-        connection.voice.setSelfDeaf(true);
-      } else if (DEF === "false") {
-        console = log(console,`account is undefend`,"error")
-        connection.voice.setSelfDeaf(false);
-      }
-      ////////////////////
-      if (MUTE === "true") {
-        console = log(console,`account is mute`,"log")
-        connection.voice.setSelfMute(true);
-      } else if (MUTE === "false") {
-        console = log(console,`account is unmute`,"error")
-        connection.voice.setSelfMute(false);
-      }
-    })
-      .catch(e => {
+    channel
+      .join()
+      .then((connection) => {
+        msssg += `[🟢] join ${channel.name}` + newLine;
+        ////////////////////
+        if (DEF === "true") {
+          msssg = msssg + `[🟢] account is defend` + newLine;
+          connection.voice.setSelfDeaf(true);
+        } else if (DEF === "false") {
+          connection.voice.setSelfDeaf(false);
+          msssg += `[⛔] account is undefend` + newLine;
+        }
+        ////////////////////
+        if (MUTE === "true") {
+          connection.voice.setSelfMute(true);
+          msssg += `[🟢] account is mute` + newLine;
+        } else if (MUTE === "false") {
+          connection.voice.setSelfMute(false);
+          msssg += `[⛔] account is unmute` + newLine;
+        }
+      })
+      .catch((e) => {
         console.error(e);
       });
-  })
+  });
   ////////////////////////////////////////////////////////////////////////////////
-  alex.on("message", async message => {
+  alex.on("message", async (message) => {
     if (!AKF || AKF !== "true") return;
-    if (message.author.bot) return;
     if (!message.guild) {
-      if (!AKF_DM_MESSAGE || AKF_DM_MESSAGE !== "true") return;
-      message.reply(`AKF: ${AKF_DM_MESSAGE}`).catch(error => {})
+      if (!AKF_DM_MESSAGE || AKF_DM_MESSAGE !== "true"|| message.author.id === alex.id) return;
+      message.reply(`AKF: ${AKF_DM_MESSAGE}`).catch((error) => {});
     } else if (message.guild) {
       if (!AKF_GUILD_MESSAGE || AKF_GUILD_MESSAGE !== "true") return;
-      message.reply(`AKF: ${AKF_GUILD_MESSAGE}`).catch(error => {})
+      message.reply(`AKF: ${AKF_GUILD_MESSAGE}`).catch((error) => {});
     } else {
       return console.error("something went wrong in my code");
     }
-  })
+  });
   alex.on("ready", async () => {
     if (!AKF || AKF !== "true") return;
     if (AKF_DM_MESSAGE || AKF_DM_MESSAGE === "true") {
-      console = log(console,`You have enabled akf dm message and the message is ${AKF_DM_MESSAGE}`,"log")
+      msssg +=
+        `[🟢] You have enabled akf dm message and the message is ${AKF_DM_MESSAGE}` +
+        newLine;
     } else if (AKF_GUILD_MESSAGE || AKF_GUILD_MESSAGE === "true") {
-      console = log(console,`You have enabled akf guild message and the message is ${AKF_GUILD_MESSAGE}`,"log")
-    };
-  })
-  console.log(console)
+      msssg +=
+        `[🟢] You have enabled akf guild message and the message is ${AKF_GUILD_MESSAGE}` +
+        newLine;
+    }
+  });
+  alex.on("ready", async () => {
+    console.log(msssg);
+  });
 }
 
-async function log(oldmsg, addmsg, cc) {
-  if (cc === "log") {
-    oldmsg = oldmsg + "🟢" + addmsg + newLine;
-  } else if (cc === "error") {
-    oldmsg = oldmsg + "⛔" + addmsg;
-  }
-  return oldmsg
-}
+
 module.exports.start = start
 module.exports.login = login
 module.exports.setStatus = setStatus
